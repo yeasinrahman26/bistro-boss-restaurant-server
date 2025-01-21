edb6a5097d53bc7fdc2a07456c8d687a8ff2c0f6
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-01-21 07:23:58 +0600

    adding dashboard and cart item

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yog8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,6 +52,20 @@ async function run() {
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)}
+      const result= await cartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+// ..........................................................
+//
+//.........................................................
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
