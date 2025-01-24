19bfd82ce3c7f1d2bd33aac86fc46d4dc216ebf3
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-01-25 05:24:03 +0600

    user colletion post and get added

--- index.js
+++ index.js
@@ -26,14 +26,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+
+    // users related apis
+    app.get("/users", async (req, res) => {
+      const result = await  userCollection.find().toArray();
+      res.send(result);
+    });
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      // insert email if user doesnt exists:
+      const query={email: user.email}
+      const existingUser=await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:"user already exists",insertedId:null })
+      }
+
+      const result= await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+
+
+
+
+
+    // menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    // review related apis
     app.get("/review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
@@ -53,19 +81,16 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/carts/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id: new ObjectId(id)}
-      const result= await cartCollection.deleteOne(query);
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-
-
-// ..........................................................
-//
-//.........................................................
-
+    // ..........................................................
+    //
+    //.........................................................
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
