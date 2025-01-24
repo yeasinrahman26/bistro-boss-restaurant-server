c1912fc670b85d9c580c0dda67401d29f9717c12
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2025-01-25 05:53:30 +0600

    user patch admin role  added

--- index.js
+++ index.js
@@ -31,37 +31,43 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
-
     // users related apis
-    app.delete('/users/:id',async(req,res)=>{
-      const id= req.params.id;
-      const query={_id: new ObjectId(id)}
-      const result= await userCollection.deleteOne(query)
-      res.send(result)
-    })
 
-
-    app.get("/users", async (req, res) => {
-      const result = await  userCollection.find().toArray();
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
-    app.post('/users',async(req,res)=>{
-      const user=req.body;
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
       // insert email if user doesnt exists:
-      const query={email: user.email}
-      const existingUser=await userCollection.findOne(query);
-      if(existingUser){
-        return res.send({message:"user already exists",insertedId:null })
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
       }
 
-      const result= await userCollection.insertOne(user)
-      res.send(result)
-    })
-
-
-
-
-
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // menu related apis
     app.get("/menu", async (req, res) => {
